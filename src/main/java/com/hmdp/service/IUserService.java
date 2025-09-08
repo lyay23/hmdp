414a635319a3258f7844d0faecc5b2434143d89f
lyay23 <3050681193@qq.com>
2025-09-08 16:37:21 +0800

    发送验证码功能，前端传入手机号，使用hutool包随机生成6为验证码，然后将验证码传入session

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,7 +1,10 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -12,5 +15,13 @@ import com.hmdp.entity.User;
  * @since 2021-12-22
  */
 public interface IUserService extends IService<User> {
+
+    /**
+     * 发送验证码功能
+     * @return
+     */
+    Result sendCode(String phone, HttpSession session);
+
+
 
 }
